b8a1079b1c794a21d895747a6fd4ebbd2f6ef8c9
Abhishek kumar <abhi.arya97161@gmail.com>
2024-03-10 19:55:19 +0530

    created all todos comp in different component

---
+++ Todo-react-app/src/Component/AllTodos.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useContext } from 'react';
+import ContextTodo from '../Store/ContextTodo';
+import styles from './AllTodos.module.css';
+
+const AllTodos = () => {
+    const ctx = useContext(ContextTodo);
+    const handleCheck = (id) => {
+        ctx.checkData(id);
+    }
+
+    return (
+        <div className={styles.container}>
+            <h4 className={styles.h4}>Todos</h4>
+            {ctx.AllData.map((todo, index) => {
+                return (
+                    <div key={todo.id} className={styles.todo}>
+                        <div className={styles.todoDetail}>
+                            <span className={styles.todo__id}>{todo.id}.</span>
+                            <span className={styles.todo__title}>{todo.title}</span>
+                            <span className={styles.todo__description}>{todo.description}</span>
+                        </div>
+
+                        <div className={styles.todoEnd}>
+                            <span><input type='checkbox' onChange={() => { handleCheck(todo.id) }} name={todo.id} className={styles.todo__checkbox}></input>
+                            </span>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    )
+}
+
+export default AllTodos;
--- Todo-react-app/src/Pages/Home/Home.jsx
+++ Todo-react-app/src/Pages/Home/Home.jsx
@@ -1,34 +1,11 @@
-import React, { useContext, useState } from 'react';
-import ContextTodo from '../../Store/ContextTodo';
-import styles from './Home.module.css';
+import React from 'react';
+import AllTodos from '../../Component/AllTodos';
 
 const Home = () => {
-  const ctx = useContext(ContextTodo);
-
-  const handleCheck = (id) => {
-    ctx.checkData(id);
-  }
-
   return (
-    <div className={styles.container}>
-      <h4 className={styles.h4}>Todos</h4>
-      {ctx.AllData.map((todo, index) => {
-        return (
-          <div key={todo.id} className={styles.todo}>
-            <div className={styles.todoDetail}>
-              <span className={styles.todo__id}>{todo.id}.</span>
-              <span className={styles.todo__title}>{todo.title}</span>
-              <span className={styles.todo__description}>{todo.description}</span>
-            </div>
-
-            <div  className={styles.todoEnd}>
-              <span><input type='checkbox' onChange={() => { handleCheck(todo.id) }} name={todo.id} className={styles.todo__checkbox}></input>
-              </span>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <AllTodos />
+    </>
   );
 }
 
